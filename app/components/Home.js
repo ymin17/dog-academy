451c81ebc7c074974fc8f455c96d8a4d1056e2b1
yr.min17@gmail.com <Lily@aggieui-macbook.home>
2020-04-01 20:07:40 -0400

    add home

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <main>
+          <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
+          <p>This seems like a nice place to get started with some Routes!</p>
+        </main>
+    </div>
+  )
+}
+
+export default Home
--- app/components/Routes.js
+++ app/components/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './Home'
 import AllCampuses from './AllCampuses'
 import AllStudents from './AllStudents'
 import SingleCampus from './SingleCampus'
@@ -16,17 +17,14 @@ const Routes = () => {
         <nav>
           Welcome!
           <ul>
+            <li><Link className="navlink" to="/">Home</Link></li>
             <li><Link className="navlink" to="/campuses">Campuses</Link></li>
             <li><Link className="navlink" to="/students">Students</Link></li>
           </ul>
         </nav>
         
-        <main>
-          <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
-          <p>This seems like a nice place to get started with some Routes!</p>
-        </main>
-        
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/campuses" component={AllCampuses} />
           <Route exact path="/campuses/addCampus" component={AddCampus} />
           <Route exact path="/students" component={AllStudents} />
